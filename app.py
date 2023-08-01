db449c290e5cb8e2c8ac74ba5abe7b7d5e289a9e
Jonah Devoy <69119958+jj862@users.noreply.github.com>
2023-08-01 13:01:01 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,86 +1,90 @@
 from __future__ import division, print_function
-# coding=utf-8
 import sys
 import os
 import glob
 import re
-import numpy as np
-import pickle
+from pathlib import Path
+from io import BytesIO
+import base64
+import requests
 
-# Keras
-from keras.applications.imagenet_utils import preprocess_input, decode_predictions
-from keras.models import load_model
-from keras.preprocessing import image
+# Import fast.ai Library
+from fastai import *
+from fastai.vision import *
 
 # Flask utils
-from flask import Flask, redirect, url_for, request, render_template
-from werkzeug.utils import secure_filename
-from gevent.pywsgi import WSGIServer
+from flask import Flask, redirect, url_for, render_template, request
+from PIL import Image as PILImage
 
 # Define a flask app
 app = Flask(__name__)
 
-# Model saved with Keras model.save()
-MODEL_PATH = 'CNN0.h5'
+NAME_OF_FILE = '/Users/jdapeman/Documents/skin2/Notebooks/CNN0' # Name of your exported file
+PATH_TO_MODELS_DIR = Path('') # by default just use /models in root dir
+classes = ['Actinic keratoses', 'Basal cell carcinoma', 'Benign keratosis',
+           'Dermatofibroma', 'Melanocytic nevi', 'Melanoma', 'Vascular lesions']
 
-# Load your trained model
-model = load_model(MODEL_PATH)
-model._make_predict_function()          # Necessary
-# print('Model loaded. Start serving...')
+def setup_model_pth(path_to_pth_file, learner_name_to_load, classes):
+    data = ImageDataBunch.single_from_classes(
+        path_to_pth_file, classes, ds_tfms=get_transforms(), size=224).normalize(imagenet_stats)
+    learn = cnn_learner(data, models.densenet169, model_dir='models')
+    learn.load(learner_name_to_load, device=torch.device('cpu'))
+    return learn
 
-# You can also use pretrained model from Keras
-# Check https://keras.io/applications/
-#from keras.applications.resnet50 import ResNet50
-#model = ResNet50(weights='imagenet')
-#model.save('')
-print('Model loaded. Check http://127.0.0.1:5000/')
+learn = setup_model_pth(PATH_TO_MODELS_DIR, NAME_OF_FILE, classes)
 
+def encode(img):
+    img = (image2np(img.data) * 255).astype('uint8')
+    pil_img = PILImage.fromarray(img)
+    buff = BytesIO()
+    pil_img.save(buff, format="JPEG")
+    return base64.b64encode(buff.getvalue()).decode("utf-8")
+	
+def model_predict(img):
+    img = open_image(BytesIO(img))
+    pred_class,pred_idx,outputs = learn.predict(img)
+    formatted_outputs = ["{:.1f}%".format(value) for value in [x * 100 for x in torch.nn.functional.softmax(outputs, dim=0)]]
+    pred_probs = sorted(
+            zip(learn.data.classes, map(str, formatted_outputs)),
+            key=lambda p: p[1],
+            reverse=True
+        )
+	
+    img_data = encode(img)
+    result = {"class":pred_class, "probs":pred_probs, "image":img_data}
+    return render_template('result.html', result=result)
+   
 
-def model_predict(img_path, model):
-    img = image.load_img(img_path, target_size=(28, 28))
-
-    # Preprocessing the image
-    x = image.img_to_array(img)
-    # x = np.true_divide(x, 255)
-    x = np.expand_dims(x, axis=0)
-
-    # Be careful how your trained model deals with the input
-    # otherwise, it won't make correct prediction!
-    x = preprocess_input(x, mode='caffe')
-
-    preds = model.predict(x)
-    return preds
-
-
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', "POST"])
 def index():
     # Main page
     return render_template('index.html')
 
 
-@app.route('/predict', methods=['GET', 'POST'])
+@app.route('/upload', methods=["POST", "GET"])
 def upload():
     if request.method == 'POST':
         # Get the file from post request
-        f = request.files['file']
-
-        # Save the file to ./uploads
-        basepath = os.path.dirname(__file__)
-        file_path = os.path.join(
-            basepath, 'uploads', secure_filename(f.filename))
-        f.save(file_path)
-
+        img = request.files['file'].read()
+        if img != None:
         # Make prediction
-        preds = model_predict(file_path, model)
-
-        # Process your result for human
-        # pred_class = preds.argmax(axis=-1)            # Simple argmax
-        pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
-        result = str(pred_class[0][0][1])               # Convert to string
-        return result
-    return None
-
+            preds = model_predict(img)
+            return preds
+    return 'OK'
+	
+@app.route("/classify-url", methods=["POST", "GET"])
+def classify_url():
+    if request.method == 'POST':
+        url = request.form["url"]
+        if url != None:
+            response = requests.get(url)
+            preds = model_predict(response.content)
+            return preds
+    return 'OK'
+    
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = os.environ.get('PORT', 8008)
 
+    if "prepare" not in sys.argv:
+        app.run(debug=False, host='0.0.0.0', port=port)
--- main.js
+++ main.js
@@ -1,54 +1,71 @@
 $(document).ready(function () {
-    // Init
-    $('.image-section').hide();
-    $('.loader').hide();
-    $('#result').hide();
 
-    // Upload Preview
-    function readURL(input) {
-        if (input.files && input.files[0]) {
-            var reader = new FileReader();
-            reader.onload = function (e) {
-                $('#imagePreview').css('background-image', 'url(' + e.target.result + ')');
-                $('#imagePreview').hide();
-                $('#imagePreview').fadeIn(650);
+    // Add the following code if you want the name of the file appear on select
+    $(".custom-file-input").change(function () {
+        var fileName = $(this).val().split("\\").pop();
+        $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+    });
+
+
+/*
+    $('#submit-url').validate({
+        rules: {
+            'url-input': {
+                required: true,
+                url: true
             }
-            reader.readAsDataURL(input.files[0]);
+        },
+        messages: {
+            'url-input': {
+                required: "This field is required",
+                url: "Please enter valid URL"
+            }
+        },
+    });
+*/
+    $('#upload-file').submit(function (event) {
+        if (document.getElementById("customFile").files.length == 0) {
+            event.preventDefault();
+            alert("Please select a file before submit");
         }
-    }
-    $("#imageUpload").change(function () {
-        $('.image-section').show();
-        $('#btn-predict').show();
-        $('#result').text('');
-        $('#result').hide();
-        readURL(this);
     });
 
-    // Predict
-    $('#btn-predict').click(function () {
-        var form_data = new FormData($('#upload-file')[0]);
+    $('#submit-url').submit(function (event) {
+        //alert("Please enter image URL before submit");
+        if (document.getElementById("url-input").value == "") {
+            event.preventDefault();
+            alert("Please enter image URL before submit");
+        }
+    });
 
-        // Show loading animation
-        $(this).hide();
-        $('.loader').show();
 
-        // Make prediction by calling api /predict
-        $.ajax({
-            type: 'POST',
-            url: '/predict',
-            data: form_data,
-            contentType: false,
-            cache: false,
-            processData: false,
-            async: true,
-            success: function (data) {
-                // Get and display the result
-                $('.loader').hide();
-                $('#result').fadeIn(600);
-                $('#result').text(' Result:  ' + data);
-                console.log('Success!');
-            },
+    /*
+        $('#btn-predict').click(function () {
+            var form_data = new FormData($('#upload-file')[0]);
+            //console.log('Success!');
+            if (document.getElementById("customFile").files.length == 0) {
+                alert("Please enter a numeric value");
+                console.log("no files selected");
+            }
+            
+            $.ajax({
+                type: 'POST',
+                url: '/upload',
+                data: form_data,
+                contentType: false,
+                cache: false,
+                processData: false,
+                async: true,
+                success: function (data) {
+                    // Get and display the result
+                    //$('.loader').hide();
+                    //$('#result').fadeIn(600);
+                    //$('#result').text(' Model Predict the Water as:  ' + data);
+                    console.log('Success!');
+                },
+            });
+            
         });
-    });
-
+        
+    */
 });
